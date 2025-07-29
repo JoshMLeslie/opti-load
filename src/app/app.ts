639a04606f893b69699b5274431ba54e9ee2488f
Josh Leslie <hello@joshmleslie.com>
2025-07-29 15:47:55 -0400

    add container to app for testing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerBuilder } from '../components/container-builder/container-builder';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContainerBuilder],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('opti-load');
